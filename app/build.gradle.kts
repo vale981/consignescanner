cf0be34f15ab7636b9d6686d210b39f636cfa1c8
Valentin Boettcher <hiro@protagon.space>
2025-05-19 14:03:20 -0400

    rev version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,9 +15,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        versionName = "1.0.0"
     }
 
     buildTypes {
