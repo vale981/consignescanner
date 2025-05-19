b30bbe0720e99c30f0be3d11348752e080ec5c04
Valentin Boettcher <hiro@protagon.space>
2025-05-19 17:28:01 -0400

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "space.protagon.consignationscanner"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildTypes {
